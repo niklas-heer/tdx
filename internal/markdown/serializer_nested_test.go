d3a525d522a52d4626a28a42e129c5810d2f1629
Niklas Heer <me@nheer.io>
2025-11-26 13:27:03 +0100

    style: fix gofmt formatting in nested test files

--- internal/markdown/serializer_nested_test.go
+++ internal/markdown/serializer_nested_test.go
@@ -18,9 +18,9 @@ func TestSerializeAST_NestedLists(t *testing.T) {
 	if err != nil {
 		t.Fatalf("ParseAST failed: %v", err)
 	}
-	
+
 	output := SerializeAST(doc)
-	
+
 	// Check that subtasks are indented with 2 spaces
 	if !strings.Contains(output, "  - [x] Subtask a") {
 		t.Errorf("Expected indented '  - [x] Subtask a', got:\n%s", output)
@@ -28,7 +28,7 @@ func TestSerializeAST_NestedLists(t *testing.T) {
 	if !strings.Contains(output, "  - [ ] Subtask b") {
 		t.Errorf("Expected indented '  - [ ] Subtask b', got:\n%s", output)
 	}
-	
+
 	// Check that top-level tasks are NOT indented
 	if strings.Contains(output, "  - [ ] Task 1") {
 		t.Errorf("Task 1 should not be indented, got:\n%s", output)
@@ -36,6 +36,6 @@ func TestSerializeAST_NestedLists(t *testing.T) {
 	if strings.Contains(output, "  - [ ] Task 3") {
 		t.Errorf("Task 3 should not be indented, got:\n%s", output)
 	}
-	
+
 	t.Logf("Serialized output:\n%s", output)
 }
